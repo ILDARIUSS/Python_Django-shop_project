e34bd7b73530f42be10a94edc0d658870c88c0ba
ildar_safarov <ildarsafarov555@gmail.com>
2026-02-13 16:08:57 +0300

    HW26.1: Product CRUD via forms, validation and styling

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
+from django.urls import reverse
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from .forms import ProductForm
 from .models import Product
 
 
@@ -21,3 +24,29 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "catalog/product_detail.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+
+    def get_success_url(self):
+        return reverse("catalog:product_detail", kwargs={"pk": self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+
+    def get_success_url(self):
+        return reverse("catalog:product_detail", kwargs={"pk": self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse("catalog:product_list")
